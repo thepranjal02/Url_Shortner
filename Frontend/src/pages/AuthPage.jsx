b120de7128f63730f0cac3d5829ed1d856b3641e
Pranjal <pranjalgangirwal14@gmail.com>
2025-09-15 14:28:13 +0530

    Frontend of my project

---
+++ Frontend/src/pages/AuthPage.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import LoginForm from '../components/LoginForm'
+import RegisterForm from '../components/RegisterForm'
+
+const AuthPage = () => {
+
+    const [login, setLogin] = useState(true)
+
+
+    return (
+        <div className="min-h-screen bg-blue-300 flex flex-col items-center justify-center p-4">
+             {login ? <LoginForm state={setLogin} /> : <RegisterForm state={setLogin} />}
+
+        </div>
+    )
+}
+
+export default AuthPage
