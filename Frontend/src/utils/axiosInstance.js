328c4f699836f4499f1a4df24556bc54ec3fa06d
Pranjal <pranjalgangirwal14@gmail.com>
2025-09-15 14:50:45 +0530

    Updated Frontend

--- Frontend/src/utils/axiosInstance.js
+++ Frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:5000",
+    baseURL:import.meta.env.VITE_API_URL,
     timeout: 10000,
     withCredentials:true
 })
